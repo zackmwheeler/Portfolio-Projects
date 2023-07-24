e97b60b764b6157bba22bc9921bea7bc7de8e218
CpnZackSparrow <140027714+CpnZackSparrow@users.noreply.github.com>
2023-07-23 23:05:41 -0700

    Add files via upload

---
+++ Hotel Revenue.sql
@@ -0,0 +1,33 @@
+-- Combining Years 2018,2019 and 2020 into one table
+
+with hotels as(
+select *
+from dbo.[2018]
+union
+select *
+from dbo.[2019]
+union
+select *
+from dbo.[2020]
+)
+
+-- Find Revenue From Year to Year and by Hotel Type
+
+
+--select 
+--	arrival_date_year, 
+--	hotel, 
+--	ROUND(SUM((stays_in_week_nights+stays_in_weekend_nights)*adr),2) as revenue
+--from hotels
+--group by 
+--	arrival_date_year, 
+--	hotel
+
+-- Add remaining tables
+
+Select *
+From hotels
+LEFT JOIN dbo.market_segment
+on hotels.market_segment = market_segment.market_segment
+LEFT JOIN dbo.meal_cost
+on hotels.meal = meal_cost.meal
